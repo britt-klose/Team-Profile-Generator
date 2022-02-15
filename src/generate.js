d9a3b3e5ee8ed0a992abc325c4c2b5ef785fd190
brittany klose <brittany.klose@yahoo.com>
2022-02-15 16:42:11 -0500

    icons for positions

--- src/generate.js
+++ src/generate.js
@@ -7,7 +7,9 @@ const addManager = function (manager) {
     <div class= "card h-100">
         <div class = "card-header">
             <h3> ${manager.name} </h3>
-         <h4> Manager </h4> <i class="put icon here" </i>
+         <h4> Manager </h4>
+         <i <span class="iconify" data-icon="foundation:torso-business"></span>
+         </i>
         </div>
 
     <div class ="card-body">
@@ -28,13 +30,15 @@ const addEngineer = function (engineer) {
     <div class= "card h-100">
     <div class = "card-header">
         <h3> ${engineer.name} </h3>
-        <h4> Engineer </h4> <i class="put icon here" </i>
+        <h4> Engineer </h4> 
+        <i <span class="iconify" data-icon="ic:twotone-engineering"></span>
+        </i>
     </div>
 
     <div class ="card-body">
         <p class="id"> ID: ${engineer.id} </p>
         <p class="email"> Email: ${engineer.email} </p>
-        <p class="username"> GitHub: ${engineer.username} </p>
+        <p class="github"> GitHub: <a target='blank' href="https://github.com/${engineer.github}">${engineer.github}</a> </p>
     </div>
     </div>
     </div>`;  
@@ -49,7 +53,9 @@ const addIntern = function(intern){
     <div class= "card h-100">
     <div class = "card-header">
         <h3> ${intern.name} </h3>
-        <h4> Manager </h4> <i class="put icon here" </i>
+        <h4> Intern </h4> 
+        <i <span class="iconify" data-icon="ph:student-bold"></span>
+        </i>
     </div>
 
     <div class ="card-body">
@@ -66,26 +72,26 @@ const addIntern = function(intern){
 
 generateHTML = (teamArray) => {
     membArr =[];
-
-    for (let i = 0; i < teamArray.length; i++) {
-        const member = teamArray[i];
-        const job = member.getJob();
+console.log(teamArray)
+    for (let i = 0; i <teamArray.length; i++) {
+        const employee = teamArray[i];
+        const job = employee.getRole();
 
         if (job === "Manager"){
-            const mngrInfo = generateManager(member);
+            const mngrInfo = addManager(employee);
             membArr.push(mngrInfo);
         }
         if (job === "Engineer"){
-            const engInfo = generateEngineer(member);
+            const engInfo = addEngineer(employee);
             membArr.push(engInfo);
         }
         if (job === "Intern"){
-            const intInfo = generateIntern(member);
+            const intInfo = addIntern(employee);
             membArr.push(intInfo);
         }
     }
 // push info together
-    const staffInfo = teamArray.join('')
+    const staffInfo = membArr.join('')
     const generateTeam = generateTeamOverview(staffInfo);
     return generateTeam;
 }
@@ -99,12 +105,13 @@ const generateTeamOverview= function (staffInfo){
 
     <head>
         <meta charset ="UTF-8">
-        <meta name ="viewport" content= "width=device-width, initial-scale=1.0>
-        <title> Team Profile Generator </title>
-        <link rel =stylesheet" href="./dist/team.css"
+        <meta name ="viewport" content= "width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
         <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link rel ="stylesheet" href="./dist/team.css">
+        <title> Team Profile Generator </title>
     </head>
 
     <body>
@@ -125,9 +132,9 @@ const generateTeamOverview= function (staffInfo){
         <script src="./script2.js"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://code.iconify.design/2/2.1.1/iconify.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
-    </html>
-    `
+    </html> `;
 }
 
 module.exports= generateHTML;

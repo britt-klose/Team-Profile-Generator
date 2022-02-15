9d233b3e8045fdf91862a7c3a370cf0a9b18aae5
brittany klose <brittany.klose@yahoo.com>
2022-02-15 13:05:59 -0500

    npm install packages

---
+++ src/generate.js
@@ -0,0 +1,9 @@
+const addManager = function (manager){
+ return `
+<div class="name">${promptData.name}</div>
+    <div class="id">${promptData.id}</div>
+    <div class="email">${promptData.email}</div>
+    <div class="username">${promptData.username}</div>
+    <div class="school">${promptData.school}</div>
+    <div class="officeNumber">${promptData.officeNumber}</div>`
+}
